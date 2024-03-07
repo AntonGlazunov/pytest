f2c5d100dc1632e96685f0311953a45b273cccde
Anton Glazunov <tagl91@yandex.ru>
2024-03-07 23:09:07 +0300

    add func & tests

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,5 +1,5 @@
 from utils import arrs
-
+from utils import dicts
 
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
@@ -13,3 +13,8 @@ def test_slice():
     assert arrs.my_slice([], 2) == []
     assert arrs.my_slice([1, 2, 3, 4], -3, -1) == [2, 3]
 
+def test_get_val():
+    assert dicts.get_val({"I": "Anton", "F": "Glazunov", "O": "Anatolevich"}, "F", "test") == "Glazunov"
+    assert dicts.get_val({"I": "Anton", "F": "Glazunov", "O": "Anatolevich"}, "a") == "git"
+    assert dicts.get_val({}, "a") == "git"
+    assert dicts.get_val({}) == "git"
